31d1897a9b61585dd1759cc3b786c22bb9dcc0e8
Marjorie Figueroa <estefi1515@gmail.com>
2019-05-19 12:48:59 +0200

    Dice problem, 2 different solutions

---
+++ dice/index.js
@@ -0,0 +1,86 @@
+/*
+    Problem is not very well explained.
+
+    Open questions
+        * If all money is lost do we keep betting 0.5 or is the game considered lost?
+        * Do we bet 0.5 each time or we bet everything as long as there is something?
+
+    Assumptions:
+        * All in for every bet as long as there is money left
+        * If all money is lost we start over with 0.5
+*/
+
+const getRandomNumber = () => Math.floor( Math.random() * 6 ) + 1;
+
+const calculateWinnings = (money) => {
+    let lost = 0;
+    for (let x = 0; x < 1000 ; x++) {
+        let number = getRandomNumber() +  getRandomNumber()
+        switch(number) {
+            case 12:
+                money *= 4;
+                break;
+            case 11:
+                money *= 3;
+                break;
+            case 10:
+                money *= 2;
+                break;
+            case 2: case 3: case 4: case 5: case 6:
+                lost += Math.abs(money);
+                money = 0;
+                break;
+            default:
+                break;
+        }
+        
+        // If all money is lost then the bet is .5c otherwise all in
+        if(money <= 0)
+            money = 0.5;
+    }
+
+    return {result: money, lost: lost};
+}
+
+const calculateWinnings2 = (money) => {
+    let lost = 0;
+    let baseBet = 0.5
+    
+    for (let x = 0; x < 10 ; x++) {
+        if(money <= 0)
+            money = 0.5;
+        let number = getRandomNumber() +  getRandomNumber()
+        switch(number) {
+            case 12:
+                money += baseBet * 4;
+                break;
+            case 11:
+                money += baseBet * 3;
+                break;
+            case 10:
+                money += baseBet * 2;
+                break;
+            case 2: case 3: case 4: case 5: case 6:
+                lost += 0.5;
+                money -= 0.5;
+                break;
+            default:
+                break;
+        }
+        console.log(number, money, lost)
+   
+    }
+
+
+    
+
+    
+
+    return {result: money, lost: lost};
+}
+
+const playTheGame = (bet) => {
+    let res = calculateWinnings(bet);
+    console.log('Result: ', res.result)
+    console.log('Lost: ', res.lost)
+}
